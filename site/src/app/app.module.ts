132d08ffca19edae4fb01d1088888f11f7b152d9
jon-fearer <fearer.jon@gmail.com>
2019-10-15 20:20:43 -0600

    gen initial components

--- site/src/app/app.module.ts
+++ site/src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AccordionModule } from 'primeng/accordion';
+import { NavBannerComponent } from './nav-banner/nav-banner.component';
+import { PostPreviewComponent } from './post-preview/post-preview.component';
+import { PostComponent } from './post/post.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBannerComponent,
+    PostPreviewComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    AccordionModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
