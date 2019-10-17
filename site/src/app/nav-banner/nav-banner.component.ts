27eea7437d20ffa6ee5e3936979eb5d660e25c9e
jon-fearer <fearer.jon@gmail.com>
2019-10-16 21:34:16 -0600

    add icons and search input

--- site/src/app/nav-banner/nav-banner.component.ts
+++ site/src/app/nav-banner/nav-banner.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-banner.component.scss']
 })
 export class NavBannerComponent implements OnInit {
+  showSearch = false;
+
+  piTagsMarginRight = 45;
+  piTagsMarginRightPx = `${this.piTagsMarginRight}px`;
+  piSearchMarginRight = 10;
+  piSearchMarginRightPx = `${this.piSearchMarginRight}px`;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  toggleSearch() {
+    this.showSearch = !this.showSearch;
+
+    if (this.showSearch) {
+      this.piTagsMarginRight += 200;
+      this.piSearchMarginRight += 200;
+    } else {
+      this.piTagsMarginRight -= 200;
+      this.piSearchMarginRight -= 200;
+    }
+
+    this.piTagsMarginRightPx = `${this.piTagsMarginRight}px`;
+    this.piSearchMarginRightPx = `${this.piSearchMarginRight}px`;
   }
 
 }
