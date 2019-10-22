c2c44161300883515ba56811ff1175a013341ff2
jon-fearer <fearer.jon@gmail.com>
2019-10-21 21:12:12 -0600

    add post text sample

--- site/src/app/post-preview/post-preview.component.ts
+++ site/src/app/post-preview/post-preview.component.ts
@@ -6,21 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-preview.component.scss']
 })
 export class PostPreviewComponent implements OnInit {
+  loremText = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, '
+    + 'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    + ' Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris '
+    + 'nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in '
+    + 'reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla'
+    + 'pariatur. Excepteur sint occaecat cupidatat non proident, sunt in '
+    + 'culpa qui officia deserunt mollit anim id est laborum.';
+
   posts = [
     {
       title: 'Doing Stuff With Typescript',
       img: '',
       path: '',
+      text: this.loremText,
     },
     {
       title: 'Doing Stuff With AWS',
       img: '',
       path: '',
+      text: this.loremText,
     },
     {
       title: 'Doing Stuff With Postgres',
       img: '',
       path: '',
+      text: this.loremText,
     },
   ];
 
