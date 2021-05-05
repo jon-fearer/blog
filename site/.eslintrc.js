204d1a46f38093b6487735194c1f8fa2f8b1e0a9
Jon Fearer <fearer.jon@gmail.com>
2021-05-04 22:43:19 -0600

    build(npm): upgrade to angular 11

--- site/.eslintrc.js
+++ site/.eslintrc.js
@@ -22,6 +22,8 @@ module.exports = {
     'require-jsdoc': 'off',
     'new-cap': 'off',
     'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': 'error'
+    '@typescript-eslint/no-unused-vars': 'error',
+    'max-len': ['error', { 'code': 120 }],
+    'object-curly-spacing': [ 'error', 'always' ],
   },
 };
--- site/src/app/app.module.ts
+++ site/src/app/app.module.ts
@@ -1,22 +1,19 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
-
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-
-import {AccordionModule} from 'primeng/accordion';
-import {NavBannerComponent} from './nav-banner/nav-banner.component';
-import {PostPreviewComponent} from './post-preview/post-preview.component';
-import {PostComponent} from './post/post.component';
-import {InputTextModule} from 'primeng/inputtext';
-import {CalendarModule} from 'primeng/calendar';
-import {TagsComponent} from './nav-banner/tags/tags.component';
-import {ButtonModule} from 'primeng/button';
-import {BioComponent} from './bio/bio.component';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AccordionModule } from 'primeng/accordion';
+import { NavBannerComponent } from './nav-banner/nav-banner.component';
+import { PostPreviewComponent } from './post-preview/post-preview.component';
+import { PostComponent } from './post/post.component';
+import { InputTextModule } from 'primeng/inputtext';
+import { CalendarModule } from 'primeng/calendar';
+import { TagsComponent } from './nav-banner/tags/tags.component';
+import { ButtonModule } from 'primeng/button';
+import { BioComponent } from './bio/bio.component';
 
 @NgModule({
   declarations: [
