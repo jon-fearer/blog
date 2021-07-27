2e7d8af63d7820bdc32df1094bf3ce56cd790be5
Jon Fearer <fearer.jon@gmail.com>
2021-07-27 14:09:43 -0600

    feat(mobile): re-add hamburger

--- site/src/app/warning/warning.component.ts
+++ site/src/app/warning/warning.component.ts
@@ -9,7 +9,8 @@ export class WarningComponent implements OnInit {
   messageContent = [{
     severity: 'warn',
     summary: 'Warning',
-    detail: 'This blog is a work in progress. Some features such as search and tags are not functional yet.',
+    detail: 'This blog is a work in progress. Some features such as search, ' +
+      'tags and mobile navigation are not functional yet.',
   }];
 
   constructor() {
