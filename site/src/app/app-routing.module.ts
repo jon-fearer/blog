cbf4656dafe1b42a1aa65a4e5c7ed4d10e632d63
Jon Fearer <fearer.jon@gmail.com>
2021-12-17 15:33:34 -0700

    fix(routing): remove base route

--- site/src/app/app-routing.module.ts
+++ site/src/app/app-routing.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RedirectComponent } from './redirect/redirect.component';
-import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  { path: '', component: AppComponent },
   { path: 'data-bowl', component: RedirectComponent },
 ];
 
