95b2221be219c55b865e7ac31f50261d6ab6c5b5
Jon Fearer <fearer.jon@gmail.com>
2021-12-17 15:03:08 -0700

    feat(data-bowl): fix test; add missing env var

--- site/src/app/redirect/redirect.component.spec.ts
+++ site/src/app/redirect/redirect.component.spec.ts
@@ -5,18 +5,39 @@ import { RedirectComponent } from './redirect.component';
 describe('RedirectComponent', () => {
   let component: RedirectComponent;
   let fixture: ComponentFixture<RedirectComponent>;
+  const assignMock = jest.fn();
+
+  beforeAll(() => {
+    delete window.location;
+    window.location = {
+      assign: assignMock,
+      ancestorOrigins: null,
+      hash: null,
+      host: null,
+      hostname: null,
+      href: null,
+      origin: null,
+      pathname: null,
+      port: null,
+      protocol: null,
+      search: null,
+      reload: null,
+      replace: null,
+    };
+  });
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [RedirectComponent],
     })
         .compileComponents();
-  });
-
-  beforeEach(() => {
     fixture = TestBed.createComponent(RedirectComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+  });
+
+  afterEach(() => {
+    assignMock.mockClear();
   });
 
   it('should create', () => {
--- site/src/environments/environment.prod.ts
+++ site/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
   contentBasePath: 'https://jonfearer.com/assets/content',
+  puntAnalysisUrl: 'https://github.com/jon-fearer/kaggle-nfl-data-bowl-2022/blob/main/punt-analysis.ipynb',
 };
