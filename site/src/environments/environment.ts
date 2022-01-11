9f3120b4da83ee4863f063ec6ae4a0d77047a6a3
Jon Fearer <fearer.jon@gmail.com>
2022-01-11 16:21:59 -0700

    feat(data-bowl): update data bowl link

--- site/src/environments/environment.ts
+++ site/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   contentBasePath: 'assets/content',
-  puntAnalysisUrl: 'https://github.com/jon-fearer/kaggle-nfl-data-bowl-2022/blob/main/punt-analysis.ipynb',
+  puntAnalysisUrl: 'https://www.kaggle.com/jonfearer/punt-analysis',
 };
